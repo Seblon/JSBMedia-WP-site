0ccdb4aef1e0f7426e16e9baae89029c4871801e
Seblon <jsbmedia@gmail.com>
2020-09-22 13:54:29 +0200

    Got Webpack up and running.

--- scripts/js/App.js
+++
@@ -1,3 +0,0 @@
-import '../../css/styles.css';
-// Delete this comment whenever
-alert('Is this thing loading?');
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
-const path = require('path'); // Import the path package from npm
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path"); // Import the path package from npm
 
-const postCSSPlugins = [
-   require('postcss-import'),
-   require('postcss-simple-vars'),
-   require('postcss-nested'),
-   require('autoprefixer'),
-];
-
-// Hit Ctrl + c to stop Webpack from watching
-module.exports = {
-   entry: './scripts/js/App.js', // starting file
-   output: {
-      filename: 'bundled.js',
-      path: path.resolve(__dirname, 'scripts'), // where bundled.js goes
-   },
-   mode: 'development',
-   watch: true, // tell Webpack to rebundle on save
+let config = {
+   entry: "./app/scripts/App.js",
    module: {
       rules: [
          {
             test: /\.css$/i,
             use: [
-               'style-loader',
-               'css-loader?url=false',
+               "style-loader",
+               "css-loader?url=false",
                {
-                  loader: 'postcss-loader',
+                  loader: "postcss-loader",
                   options: {
                      postcssOptions: {
-                        plugins: [postCSSPlugins],
+                        plugins: [
+                           require("postcss-import"),
+                           require("postcss-mixins")(),
+                           require("postcss-simple-vars")(),
+                           require("postcss-nested")(),
+                           require("autoprefixer")(),
+                        ],
                      },
                   },
                },
@@ -36,3 +29,29 @@ module.exports = {
       ],
    },
 };
+
+if (currentTask == "dev") {
+   config.output = {
+      filename: "bundled.js",
+      path: path.resolve(__dirname, "app"), // where bundled.js goes
+   };
+   config.devServer = {
+      before: function (app, server) {
+         server._watch("./app/**/*.html");
+      },
+      contentBase: path.join(__dirname, "app"),
+      hot: true,
+      port: 3000,
+      host: "0.0.0.0",
+   };
+   config.mode = "development";
+}
+if (currentTask == "build") {
+   config.output = {
+      filename: "bundled.js",
+      path: path.resolve(__dirname, "dist"), // where bundled.js goes
+   };
+   config.mode = "production";
+}
+
+module.exports = config;
